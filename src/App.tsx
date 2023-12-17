8d587f8fa2f53c63bb9a84ae5021ddd651cf24e9
Adam Bridgers <16521414+awbridgers@users.noreply.github.com>
2023-12-16 20:53:41 -0500

    basic board layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,54 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import styled from 'styled-components';
+import {Chess} from 'chess.js';
+import {useRef} from 'react';
 
 function App() {
+  const chess = useRef(new Chess());
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Container>
+      <div className="title">Tactics Trainer</div>
+      <div className="board">
+        {chess.current &&
+          chess.current.board().map((row, i) => (
+            <BoardRow>
+              {row.map((square, j) => (
+                <BoardSquare row={i} col={j} />
+              ))}
+            </BoardRow>
+          ))}
+      </div>
+    </Container>
   );
 }
+
+const BoardRow = styled.div`
+  display: flex;
+  flex-direction: row;
+`;
+const BoardSquare = styled.div<{row: number; col: number}>`
+  background-color: ${(props) =>
+    props.row % 2 === 1
+      ? props.col % 2 === 0
+        ? '#B88762'
+        : '#EDD6B0'
+      : props.col % 2 === 0
+      ? '#EDD6B0'
+      : '#B88762'};
+  height: 50px;
+  width: 50px;
+`;
+const Container = styled.div`
+  text-align: center;
+  height: 100vh;
+  .title {
+    font-size: 40px;
+    color: white;
+  }
+  .board {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+  }
+`;
 
 export default App;
