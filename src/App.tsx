b805f495d739807908f84b43bfbffac55e6d8666
Adam Bridgers <16521414+awbridgers@users.noreply.github.com>
2023-12-18 22:13:15 -0500

    adds the basic game structure: load tactic, allow the user to make moves, only continue to the next move if the user move is correct

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import styled from 'styled-components';
-import {firebaseConfig} from './firebaseConfig'
-import { initializeApp } from 'firebase/app';
-import {Database, get, getDatabase, ref} from 'firebase/database'
+import {firebaseConfig} from './firebaseConfig';
+import {initializeApp} from 'firebase/app';
+import {Database, get, getDatabase, ref} from 'firebase/database';
 import {Chess, PieceSymbol, Square, Move} from 'chess.js';
 import React, {useEffect, useRef, useState} from 'react';
 import blackPawn from './images/blackPawn2.png';
@@ -17,13 +17,13 @@ import whiteBishop from './images/whiteBishop2.png';
 import whiteQueen from './images/whiteQueen2.png';
 import whiteKing from './images/whiteKing2.png';
 import Promo from './Components/Promo';
-import { PGNFormat, PGNMove } from './types';
+import {PGNFormat, PGNMove} from './types';
 
 const app = initializeApp(firebaseConfig);
-const blankPGN:PGNFormat = {
+const blankPGN: PGNFormat = {
   fen: '8/8/8/8/8/8/8/8 w - - 0 1',
-  pgn: []
-}
+  pgn: [],
+};
 const getImage = (piece: PieceSymbol, color: 'w' | 'b') => {
   switch (piece) {
     case 'p':
@@ -43,8 +43,6 @@ const getImage = (piece: PieceSymbol, color: 'w' | 'b') => {
   }
 };
 
-
-
 function App() {
   const [selectedSquare, setSelectedSquare] = useState<Square | null>(null);
   const [targetSquares, setTargetSquares] = useState<Map<
@@ -52,18 +50,21 @@ function App() {
     Omit<Move, 'to'>
   > | null>(null);
   const [showPromo, setShowPromo] = useState<boolean>(false);
-  const [playerColor, setPlayerColor] = useState<'w'|'b'>('w')
+  const [playerColor, setPlayerColor] = useState<'w' | 'b'>('w');
   const [playerMove, setPlayerMove] = useState<boolean>(false);
-  const [solution, setSolution] = useState<PGNMove[]>([])
+  const [solution, setSolution] = useState<PGNMove[]>([]);
   const [tacticActive, setTacticActive] = useState<boolean>(false);
   const [attemptedMove, setAttemptedMove] = useState<string | null>(null);
-  const [showSolution, setShowSolution] = useState<boolean>(false)
-  const [loadNewTactic, setLoadNewTactic] = useState<boolean>(true)
+  const [showSolution, setShowSolution] = useState<boolean>(false);
+  const [moveResult, setMoveResult] = useState<boolean | null>(null);
+  const [loadNewTactic, setLoadNewTactic] = useState<boolean>(true);
   const promoInfo = useRef<{to: Square; from: Square} | null>(null);
-  const chess = useRef(new Chess('rk1q4/ppp2P1P/8/8/8/8/6P1/3QK2R w K - 0 1'));
-  const db = useRef<Database>(getDatabase(app))
+  const chess = useRef(new Chess());
+  const db = useRef<Database>(getDatabase(app));
   const currentTactic = useRef<PGNFormat>(blankPGN);
+  const moveHistory = useRef<string>('');
 
+  //handle the seleciton of squares for the movement of pieces
   const handleClick = (e: React.MouseEvent) => {
     if (!selectedSquare) {
       const squareInfo = chess.current.get(e.currentTarget.id as Square);
@@ -79,8 +80,9 @@ function App() {
       }
     } else {
       if (targetSquares?.has(e.currentTarget.id as Square)) {
-        console.log('test');
         //this is a legal move!
+        //store the current fen so we can comeback to it later
+        moveHistory.current = chess.current.fen();
         const info = targetSquares.get(e.currentTarget.id as Square);
         if (info && info.promotion) {
           setShowPromo(true);
@@ -89,37 +91,41 @@ function App() {
             to: e.currentTarget.id as Square,
           };
         } else {
-          chess.current.move({from: selectedSquare, to: e.currentTarget.id});
+          movePiece(selectedSquare, e.currentTarget.id as Square, 'q');
         }
-        
       }
       setSelectedSquare(null);
-        setTargetSquares(null);
+      setTargetSquares(null);
     }
   };
+  const movePiece = (from: Square, to: Square, promotion: string) => {
+    //store the previous position
+    moveHistory.current = chess.current.fen();
+    const move = chess.current.move({to, from, promotion});
+    setAttemptedMove(move.san);
+  };
+  //handle the promotion of pieces
   const promotePiece = (e: React.MouseEvent) => {
-    console.log(e.currentTarget.id);
-
     if (promoInfo.current) {
-      console.log(promoInfo.current);
       const {to, from} = promoInfo.current;
-      chess.current.move({to, from, promotion: e.currentTarget.id});
+      movePiece(to, from, e.currentTarget.id);
       setShowPromo(false);
-
       promoInfo.current = null;
     }
   };
+  //cancel a move that involves a promotion
   const cancelPromotion = () => {
     setSelectedSquare(null);
     setTargetSquares(null);
     setShowPromo(false);
     promoInfo.current = null;
   };
-  useEffect(()=>{
-    const loadTactic = async ()=>{
+  //load a tactic
+  useEffect(() => {
+    const loadTactic = async () => {
       const random = Math.floor(Math.random() * 4740); //NOTE: 4740 is CURRENT number of tactics in the DB
       const fetch = await get(ref(db.current, `tacticsList/${random}`));
-      const tactic:PGNFormat = await fetch.val();
+      const tactic: PGNFormat = await fetch.val();
       chess.current.load(tactic.fen);
       setPlayerColor(chess.current.turn());
       setAttemptedMove(null);
@@ -127,17 +133,65 @@ function App() {
       setSolution(tactic.pgn);
       setTacticActive(true);
       currentTactic.current = tactic;
-
-    }
-    if(loadNewTactic){
+    };
+    if (loadNewTactic) {
       loadTactic();
-      setLoadNewTactic(false)
+      setLoadNewTactic(false);
     }
-  },[loadNewTactic])
+  }, [loadNewTactic]);
+  //Determine if the player's move is correct
+  useEffect(() => {
+    if (attemptedMove && tacticActive) {
+      const theAnswer = [...solution];
+      if (theAnswer[0].move.includes(attemptedMove)) {
+        //this move is correct!
+        setMoveResult(true);
+        if (theAnswer.length === 1) {
+          //the last move of the solution
+          setTacticActive(false);
+          setShowSolution(false);
+        } else {
+          //there are more moves in the solution
+          theAnswer.shift();
+          setSolution(theAnswer);
+          setPlayerMove(false);
+          setAttemptedMove(null);
+        }
+      } else {
+        //the move is incorrect
+        setMoveResult(false);
+        setTimeout(() => {
+          chess.current.load(moveHistory.current);
+          setAttemptedMove(null);
+          setPlayerMove(true);
+        }, 1000);
+      }
+    }
+  }, [attemptedMove, tacticActive, solution]);
+  //the computer move
+  useEffect(() => {
+    if (!playerMove && tacticActive) {
+      const theAnswer = [...solution];
+      //the computer makes the next move in the solution
+      const computerMove = theAnswer.shift();
+      if (computerMove) {
+        setTimeout(() => {
+          chess.current.move(computerMove.move);
+          setSolution(theAnswer);
+          setPlayerMove(true);
+        }, 500);
+      }
+    }
+  }, [playerMove, tacticActive, solution]);
   return (
     <Container>
       <div className="title">Tactics Trainer</div>
+
       <div className="board">
+        <div className="subTitle">
+          {playerColor === 'w' ? 'White' : 'Black'} to play
+        </div>
+
         {chess.current &&
           chess.current.board().map((row, i) => (
             <BoardRow key={i}>
@@ -145,8 +199,15 @@ function App() {
                 const id = `${String.fromCharCode(j + 97)}${8 - i}`;
                 return (
                   <BoardSquare
-                    $row={i}
-                    $col={j}
+                    $squareColor={
+                      i % 2 === 1
+                        ? j % 2 === 0
+                          ? 'dark'
+                          : 'light'
+                        : j % 2 === 0
+                        ? 'light'
+                        : 'dark'
+                    }
                     key={j}
                     $selected={selectedSquare === id}
                     $targetted={
@@ -156,6 +217,8 @@ function App() {
                     onClick={handleClick}
                     //onDragEnter={handleDragEnter}
                   >
+                    {i === 7 && <div className="file">{id[0]}</div>}
+                    {j === 0 && <div className="rank">{id[1]}</div>}
                     {square && (
                       <img
                         src={getImage(square.type, square.color)}
@@ -176,29 +239,32 @@ function App() {
               })}
             </BoardRow>
           ))}
+        <Results $color={!!moveResult}>
+          {moveResult === false && <>Incorrect, keep trying!</>}
+          {moveResult === true && <>Great Job!</>}
+        </Results>
       </div>
     </Container>
   );
 }
-
+const Results = styled.div<{$color: boolean}>`
+  height: 50px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  font-size: 20px;
+  color: ${(props) => (props.$color ? 'green' : 'red')};
+`;
 const BoardRow = styled.div`
   display: flex;
   flex-direction: row;
 `;
 const BoardSquare = styled.div<{
-  $row: number;
-  $col: number;
+  $squareColor: 'dark' | 'light';
   $selected: boolean;
   $targetted: boolean;
 }>`
-  background-color: ${(props) =>
-    props.$row % 2 === 1
-      ? props.$col % 2 === 0
-        ? '#B88762'
-        : '#EDD6B0'
-      : props.$col % 2 === 0
-      ? '#EDD6B0'
-      : '#B88762'};
+  background-color: ${props=>props.$squareColor === 'light' ? '#EDD6B0': '#B88762'};
   outline: ${(props) =>
     props.$selected
       ? '4px solid yellow'
@@ -213,9 +279,20 @@ const BoardSquare = styled.div<{
   display: flex;
   justify-content: center;
   align-items: center;
+  color: ${props=>props.$squareColor === 'light' ? '#B88762' : '#EDD6B0'};
   img {
     width: 80%;
     margin: auto;
+  }
+  .file {
+    position: absolute;
+    bottom: 2px;
+    right: 2px;
+  }
+  .rank {
+    position: absolute;
+    top: 2px;
+    left: 2px;
   }
 `;
 const Container = styled.div`
@@ -229,6 +306,10 @@ const Container = styled.div`
     display: flex;
     flex-direction: column;
     align-items: center;
+  }
+  .subTitle {
+    font-size: 25px;
+    color: white;
   }
 `;
 
